a1b212806a64a167dd8ca8326d9a325fcd4de22a
augus <augus@qq.com>
2021-12-02 14:51:17 +0800

    second commit

--- src/main/java/com/augus/git/GitTest.java
+++ src/main/java/com/augus/git/GitTest.java
@@ -13,5 +13,6 @@ package com.augus.git;/**
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }

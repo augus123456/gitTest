386a4034ed93c07b9fc55364e82f98999a3ae752
augus <augus@qq.com>
2021-12-02 14:49:52 +0800

    first commit

---
+++ src/main/java/com/augus/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.augus.git;/**
+ * @author augus
+ * @create 2021-12-02-14:47
+ */
+
+/**
+ * @ClassName:GitTest
+ * @Description:TODO
+ * @author Augus
+ * @date 2021/12/2 14:47
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
